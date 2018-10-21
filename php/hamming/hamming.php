77aa575340075bec65f1df857ebfc16667acb6db
Samuel Huang <samhuang91@gmail.com>
2018-10-21 09:04:59 -0700

    Implement hamming solution

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,13 +1,17 @@
 <?php
 
-//
-// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 function distance($a, $b)
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (strlen($a) != strlen($b)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+
+    $result = 0;
+    for ($i = 0; $i < strlen($a); $i++) {
+        if ($a[$i] != $b[$i]) {
+            $result += 1;
+        }
+    }
+
+    return $result;
 }
