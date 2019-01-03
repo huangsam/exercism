432b16ec5cfd6dc1639213363e59aa4e2cdeadf6
Samuel Huang <samhuang91@gmail.com>
2019-01-03 11:31:57 -0800

    Implement leap in javascript

---
+++ javascript/leap/leap.js
@@ -0,0 +1,8 @@
+export function isLeap(year) {
+    if (year % 4 != 0) {
+        return false;
+    } else if (year % 100 == 0 && year % 400 != 0) {
+        return false;
+    }
+    return true;
+}
