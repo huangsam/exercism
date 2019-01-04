3a826bcf4e17089977ebc13b0a5da602d0bb69bb
Samuel Huang <samhuang91@gmail.com>
2019-01-03 21:38:10 -0800

    Reformat golang code

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -5,7 +5,7 @@ import "strings"
 
 // Abbreviate receives a full string and returns an acronym.
 func Abbreviate(s string) string {
-    var sb strings.Builder
+	var sb strings.Builder
 	spaceTokens := strings.Split(s, " ")
 	for _, spaceToken := range spaceTokens {
 		if spaceToken == "-" {
@@ -13,12 +13,12 @@ func Abbreviate(s string) string {
 		}
 		dashTokens := strings.Split(spaceToken, "-")
 		if len(dashTokens) == 0 {
-            sb.WriteByte(spaceToken[0])
+			sb.WriteByte(spaceToken[0])
 		} else {
 			for _, dashToken := range dashTokens {
-                sb.WriteByte(dashToken[0])
+				sb.WriteByte(dashToken[0])
 			}
 		}
 	}
-    return strings.ToUpper(sb.String())
+	return strings.ToUpper(sb.String())
 }
--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -6,17 +6,17 @@ type Planet string
 var earthYearInSeconds = 31557600.00
 
 var earthYearMultiples = map[Planet]float64{
-    "Earth": 1.000000,
-    "Mercury": 0.2408467,
-    "Venus": 0.61519726,
-    "Mars": 1.8808158,
-    "Jupiter": 11.862615,
-    "Saturn": 29.447498,
-    "Uranus": 84.016846,
-    "Neptune": 164.79132,
+	"Earth":   1.000000,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
 }
 
 // Age returns relative age on desired planet
 func Age(seconds float64, planet Planet) float64 {
-    return seconds / earthYearInSeconds / earthYearMultiples[planet]
+	return seconds / earthYearInSeconds / earthYearMultiples[planet]
 }
