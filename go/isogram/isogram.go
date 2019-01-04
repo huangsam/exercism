c2f2fed52b3b7c37aa684e148873ac651fb1f7c4
Samuel Huang <samhuang91@gmail.com>
2019-01-04 08:55:28 -0800

    Implement isogram in go

---
+++ go/isogram/isogram.go
@@ -0,0 +1,24 @@
+package isogram
+
+import "unicode"
+
+func IsIsogram(word string) bool {
+	seen := make(map[rune]int)
+	for _, ch := range word {
+        if ch == '-' || ch == ' ' {
+            continue
+        }
+        newCh := unicode.ToLower(ch)
+		if _, ok := seen[newCh]; ok {
+			seen[newCh] += 1
+		} else {
+			seen[newCh] = 1
+		}
+	}
+	for _, count := range seen {
+		if count > 1 {
+			return false
+		}
+	}
+	return true
+}
