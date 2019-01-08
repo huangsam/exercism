bc5446f9564a2ac83278074b1d26e4a2b7d089d5
Samuel Huang <samhuang91@gmail.com>
2019-01-08 01:14:10 -0800

    Fix gigasecond signature for cpp

--- cpp/gigasecond/gigasecond.cpp
+++ cpp/gigasecond/gigasecond.cpp
@@ -1,8 +1,9 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
+
 using namespace boost::posix_time;
 
 namespace gigasecond {
-    const ptime advance(ptime current) {
+    ptime advance(const ptime& current) {
         return current + seconds(1000000000);
     }
 }
--- cpp/gigasecond/gigasecond.h
+++ cpp/gigasecond/gigasecond.h
@@ -2,10 +2,11 @@
 #define GIGASECOND_H
 
 #include "boost/date_time/posix_time/posix_time.hpp"
+
 using namespace boost::posix_time;
 
 namespace gigasecond {
-    const ptime advance(ptime current);
+    ptime advance(const ptime& current);
 }
 
 #endif
