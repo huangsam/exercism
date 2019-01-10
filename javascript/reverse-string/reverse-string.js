0007ccdce62f931c7d2928c48dd6f9a2f2a43145
Samuel Huang <samhuang91@gmail.com>
2019-01-09 22:13:03 -0800

    Minimize substr call in reverse-string

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -2,5 +2,5 @@ export function reverseString(seq) {
     if (seq.length <= 1) {
         return seq;
     }
-    return reverseString(seq.substr(1, seq.length)) + seq[0];
+    return reverseString(seq.substr(1)) + seq[0];
 }
