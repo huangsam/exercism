68f7e8968c7ff52cadef2a1853ece4893c41967f
Samuel Huang <samhuang91@gmail.com>
2019-01-12 18:26:30 -0800

    Implement etl in c++

---
+++ cpp/etl/etl.h
@@ -0,0 +1,32 @@
+#if !defined(ETL_H)
+#define ETL_H
+
+#include <map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+namespace etl {
+
+    char tolower(char in) {
+        if ('A' <= in && in <= 'Z') {
+            return in - ('Z' - 'z');
+        }
+        return in;
+    }
+
+    map<char, int> transform(const map<int, vector<char>> old) {
+        map<char, int> result;
+        for (auto it = old.cbegin(); it != old.cend(); it++) {
+            int points = it->first;
+            vector<char> chars = it->second;
+            for (auto sit = chars.cbegin(); sit != chars.cend(); sit++) {
+                result[tolower(*sit)] = points;
+            }
+        }
+        return result;
+    }
+}
+
+#endif
