232793f6e9833a73a6d514aae7489b5e28a9b00c
Samuel Huang <samhuang91@gmail.com>
2019-01-14 08:36:10 -0800

    Implement difference in cpp

---
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,14 @@
+namespace squares {
+    int square_of_sum(int x) {
+        int sum = x * (x + 1) / 2;
+        return sum * sum;
+    }
+
+    int sum_of_squares(int x) {
+        return x * (x + 1) * (2 * x + 1) / 6;
+    }
+
+    int difference(int x) {
+        return square_of_sum(x) - sum_of_squares(x);
+    }
+}
