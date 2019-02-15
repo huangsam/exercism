d3d816e40461576231f81ed6850d639f9dcbcdca
Samuel Huang <samhuang91@gmail.com>
2019-02-15 14:48:41 -0800

    Refine vars and cleanup loop

--- go/flatten-array/flatten.go
+++ go/flatten-array/flatten.go
@@ -2,19 +2,18 @@ package flatten
 
 // Flatten processes lists with integers and nested lists
 func Flatten(in interface{}) (out []interface{}) {
-	outSlice := make([]interface{}, 0)
+	result := make([]interface{}, 0)
 
 	for _, v := range in.([]interface{}) {
 		switch v.(type) {
 		case []interface{}:
-			flatSlice := Flatten(v)
-			outSlice = append(outSlice, flatSlice...)
+			result = append(result, Flatten(v)...)
 		case nil:
 			continue
 		default:
-			outSlice = append(outSlice, v)
+			result = append(result, v)
 		}
 	}
 
-	return outSlice
+	return result
 }
