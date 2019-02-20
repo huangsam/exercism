1197e0e9bf873bef68fab096c1b6ae98c3e4f0fd
Samuel Huang <samhuang91@gmail.com>
2019-02-19 21:05:22 -0800

    Implement accumulate in go

---
+++ go/accumulate/accumulate.go
@@ -0,0 +1,11 @@
+// Package accumulate applies string conversion to slices
+package accumulate
+
+// Accumulate transforms a slice of string data
+func Accumulate(in []string, converter func(string) string) []string {
+	var out []string
+	for _, v := range in {
+		out = append(out, converter(v))
+	}
+	return out
+}
