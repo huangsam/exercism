de5e8471204d3ae5a505d388b54cc11e4d788771
Samuel Huang <samhuang91@gmail.com>
2019-02-19 21:17:04 -0800

    Implement etl in go

---
+++ go/etl/etl.go
@@ -0,0 +1,17 @@
+// Package etl provides utilities for data cleanup
+package etl
+
+import (
+	"strings"
+)
+
+// Transform converts old map format to new map format
+func Transform(in map[int][]string) map[string]int {
+	out := make(map[string]int)
+	for k, v := range in {
+		for _, w := range v {
+			out[strings.ToLower(w)] = k
+		}
+	}
+	return out
+}
