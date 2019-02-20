2ec4e14b9f7f437474e48e131a8592772c2daa1e
Samuel Huang <samhuang91@gmail.com>
2019-02-19 22:38:36 -0800

    Implement rna-transcription in go

---
+++ go/rna-transcription/rna_transcription.go
@@ -0,0 +1,21 @@
+// Package strand deals with DNA and RNA
+package strand
+
+import (
+	"strings"
+)
+
+// ToRNA converts DNA sequence to RNA sequence
+func ToRNA(dna string) string {
+	var builder strings.Builder
+	transform := map[rune]rune{
+		'C': 'G',
+		'G': 'C',
+		'T': 'A',
+		'A': 'U',
+	}
+	for _, v := range dna {
+		builder.WriteRune(transform[v])
+	}
+	return builder.String()
+}
