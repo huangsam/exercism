a0aca902b2ecc1981050dcbfda0f7815317accea
Samuel Huang <samhuang91@gmail.com>
2019-03-11 09:03:08 -0700

    Fixup hamming for js

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,12 +1,12 @@
 export function compute(a, b) {
-    if (a.length !== b.length) {
-        throw new Error('left and right strands must be of equal length');
+  if (a.length !== b.length) {
+    throw new Error('left and right strands must be of equal length');
+  }
+  let result = 0;
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      result += 1;
     }
-    let result = 0;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            result += 1;
-        }
-    }
-    return result;
+  }
+  return result;
 }
