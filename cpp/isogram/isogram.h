10acb65e71b2c6e9fce7789b67e1fa9776313d1f
Samuel Huang <samhuang91@gmail.com>
2019-03-19 17:29:57 -0700

    Implement isogram in cpp

---
+++ cpp/isogram/isogram.h
@@ -0,0 +1,28 @@
+#if !defined(ISOGRAM_H)
+#define ISOGRAM_H
+
+#include <cctype>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+namespace isogram {
+
+    bool is_isogram(const string& input) {
+        bool c[26] = {false};
+        for (const auto& it : input) {
+            if (isalpha(it)) {
+                int cid = (int) (tolower(it) - 'a');
+                if (c[cid]) {
+                    return false;
+                }
+                c[cid] = true;
+            }
+        }
+        return true;
+    }
+
+}
+
+#endif
