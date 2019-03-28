83a4672e393921c86126c5ad6e9a47abf82f066b
Samuel Huang <samhuang91@gmail.com>
2019-03-28 08:19:53 -0700

    Convert to two-liner

--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -12,7 +12,8 @@ namespace triangle {
     enum flavor { equilateral, isosceles, scalene };
 
     bool is_invalid(double a, double b, double c) {
-        return a <= 0 || b <= 0 || c <= 0 || a+b < c || b+c < a || c+a < b;
+        return (a <= 0 || b <= 0 || c <= 0) \
+            || (a+b < c || b+c < a || c+a < b);
     }
 
     bool is_equilateral(double a, double b, double c) {
