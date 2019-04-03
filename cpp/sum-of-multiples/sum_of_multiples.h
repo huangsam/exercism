3b794c1de20add6f1504bbc5e5d864f18f17ca36
Samuel Huang <samhuang91@gmail.com>
2019-04-03 11:55:40 -0700

    Implement sum-of-multiples in cpp

--- cpp/sum-of-multiples/sum_of_multiples.h
+++ cpp/sum-of-multiples/sum_of_multiples.h
@@ -1,13 +1,23 @@
 #if !defined(SUM_OF_MULTIPLES_H)
 #define SUM_OF_MULTIPLES_H
 
+#define EXERCISM_RUN_ALL_TESTS
+
 #include <set>
 
 namespace sum_of_multiples {
 
     int to(const std::set<int> integers, int number) {
-        if (integers.size()) return 0;
-        return number;
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            for (const auto &j : integers) {
+                if (i % j == 0) {
+                    sum += i;
+                    break;
+                }
+            }
+        }
+        return sum;
     }
 
 }
