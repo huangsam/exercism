cfa859d54f662aaeeb302fd8211f664a5a1b162e
Samuel Huang <samhuang91@gmail.com>
2019-04-04 23:33:35 -0700

    Implement grains in cpp

---
+++ cpp/grains/grains.h
@@ -0,0 +1,22 @@
+#if !defined(GRAINS_H)
+#define GRAINS_H
+
+#define EXERCISM_RUN_ALL_TESTS
+
+#include <cstdint>
+
+namespace grains {
+
+    typedef uint_fast64_t grains_count;
+
+    grains_count square(int n) {
+        return grains_count(1) << (n-1);
+    }
+
+    grains_count total() {
+        return ~static_cast<grains_count>(0);
+    }
+
+}
+
+#endif
