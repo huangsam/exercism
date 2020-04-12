79264b8233cefd22ed20a2d19182402d67119be5
Samuel Huang <samhuang91@gmail.com>
2020-04-11 22:46:07 -0700

    Reorder conditionals for bob in go

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -2,36 +2,35 @@ package bob
 
 // Hey receives a remark and returns a response.
 func Hey(remark string) string {
-	lastQuestion := -1
 	lastUpper := -1
 	lastLower := -1
 	lastNumber := -1
+	lastQuestion := -1
 
 	for i := 0; i < len(remark); i++ {
-		if 'a' <= remark[i] && remark[i] <= 'z' {
-			lastLower = i
-		} else if 'A' <= remark[i] && remark[i] <= 'Z' {
+		if 'A' <= remark[i] && remark[i] <= 'Z' {
 			lastUpper = i
-		} else if remark[i] == '?' {
-			lastQuestion = i
+		} else if 'a' <= remark[i] && remark[i] <= 'z' {
+			lastLower = i
 		} else if '0' <= remark[i] && remark[i] <= '9' {
 			lastNumber = i
+		} else if remark[i] == '?' {
+			lastQuestion = i
 		}
 	}
 
-	isQuestion := lastQuestion > lastUpper && lastQuestion > lastLower && lastQuestion > lastNumber
+	isAsking := lastQuestion > lastUpper && lastQuestion > lastLower && lastQuestion > lastNumber
 	isYelling := lastUpper > -1 && lastLower == -1
-	isSomething := lastUpper > -1 || lastLower > -1 || lastNumber > -1
+	isSilent := lastUpper == -1 && lastLower == -1 && lastNumber == -1
 
-	if isQuestion {
+	if isAsking {
 		if isYelling {
 			return "Calm down, I know what I'm doing!"
-		} else {
-			return "Sure."
 		}
+		return "Sure."
 	} else if isYelling {
 		return "Whoa, chill out!"
-	} else if !isSomething {
+	} else if isSilent {
 		return "Fine. Be that way!"
 	}
 	return "Whatever."
