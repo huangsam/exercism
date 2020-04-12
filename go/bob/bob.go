2f5886d9b7d5df1fab0b243843bd72d947ea71c4
Samuel Huang <samhuang91@gmail.com>
2020-04-11 20:01:13 -0700

    Optimize bob in go

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,47 +1,41 @@
 package bob
 
-import "strings"
-
 // Hey receives a remark and returns a response.
 func Hey(remark string) string {
-    questionMark := false
-    upperCaseFound := false
-    lowerCaseFound := false
-    numbersFound := false
-    charsAfterQuestion := false
+	lastQuestion := -1
+	lastUpper := -1
+	lastLower := -1
+	lastNumber := -1
 
-    for i := 0; i < len(remark); i++ {
-        if 'a' <= remark[i] && remark[i] <= 'z' {
-            lowerCaseFound = true
-        } else if 'A' <= remark[i] && remark[i] <= 'Z' {
-            upperCaseFound = true
-        } else if remark[i] == '?' {
-            questionMark = true
-        } else if '0' <= remark[i] && remark[i] <= '9' {
-            numbersFound = true
-        }
-    }
+	for i := 0; i < len(remark); i++ {
+		if 'a' <= remark[i] && remark[i] <= 'z' {
+			lastLower = i
+		} else if 'A' <= remark[i] && remark[i] <= 'Z' {
+			lastUpper = i
+		} else if remark[i] == '?' {
+			lastQuestion = i
+		} else if '0' <= remark[i] && remark[i] <= '9' {
+			lastNumber = i
+		}
+	}
 
-    if questionMark == true {
-        newRemark := strings.TrimSpace(remark)
-        if newRemark[len(newRemark) - 1] != '?' {
-            charsAfterQuestion = true
-        }
-    }
+	charsAfterQuestion := lastQuestion <= lastUpper ||
+		lastQuestion <= lastLower ||
+		lastQuestion <= lastNumber
 
-    isYelling := upperCaseFound && !lowerCaseFound
-    saidSomething := upperCaseFound || lowerCaseFound || numbersFound
+	isYelling := lastUpper > -1 && lastLower == -1
+	saidSomething := lastUpper > -1 || lastLower > -1 || lastNumber > -1
 
-    if questionMark {
-        if isYelling {
-            return "Calm down, I know what I'm doing!"
-        } else if !charsAfterQuestion {
-            return "Sure."
-        }
-    } else if isYelling {
-        return "Whoa, chill out!"
-    } else if !saidSomething {
-        return "Fine. Be that way!"
-    }
+	if lastQuestion > -1 {
+		if isYelling {
+			return "Calm down, I know what I'm doing!"
+		} else if !charsAfterQuestion {
+			return "Sure."
+		}
+	} else if isYelling {
+		return "Whoa, chill out!"
+	} else if !saidSomething {
+		return "Fine. Be that way!"
+	}
 	return "Whatever."
 }
