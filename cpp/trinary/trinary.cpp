863c9a56f4aee79d603358d66f94caac6bf61866
Samuel Huang <samhuang91@gmail.com>
2020-05-26 08:54:41 -0700

    Remove std::pow from trinary in cpp

--- cpp/trinary/trinary.cpp
+++ cpp/trinary/trinary.cpp
@@ -1,15 +1,14 @@
 #include "trinary.h"
-#include <cmath>
 
 namespace trinary {
 
     int to_decimal(const std::string &input) {
         int sum = 0;
-        int power = 0;
-        for (auto it = input.rbegin(); it != input.rend(); it++, power++) {
+        int factor = 1;
+        for (auto it = input.rbegin(); it != input.rend(); it++, factor *= 3) {
             int num = (int) (*it - '0');
             if (num < 0 || num > 2) return 0;
-            sum += (int) (num * std::pow(3, power));
+            sum += num * factor;
         }
         return sum;
     }
