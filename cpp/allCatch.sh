a8638b7e2d7567805cb5d1228d293be7e1e0879e
Samuel Huang <samhuang91@gmail.com>
2020-05-26 08:44:13 -0700

    Move allCatch into allSetup

--- cpp/allCatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-TEMPLATE_NAME=".bootstrap"
-TEMPLATE_DIR="$(pwd)/$TEMPLATE_NAME"
-CATCH_VERSION="v2.11.3"
-CATCH_URL="https://github.com/catchorg/Catch2/releases/download/$CATCH_VERSION/catch.hpp"
-curl -sSL "$CATCH_URL" -o "$TEMPLATE_DIR/test/catch.hpp"
--- cpp/allSetup.sh
+++ cpp/allSetup.sh
@@ -2,6 +2,12 @@
 set -eu
 TEMPLATE_NAME=".bootstrap"
 TEMPLATE_DIR="$(pwd)/$TEMPLATE_NAME"
+CATCH_VERSION="v2.11.3"
+CATCH_URL="https://github.com/catchorg/Catch2/releases/download/$CATCH_VERSION/catch.hpp"
+CATCH_PATH="$TEMPLATE_DIR/test/catch.hpp"
+if [[ ! -f $CATCH_PATH ]]; then
+    curl -sSL "$CATCH_URL" -o "$CATCH_PATH"
+fi
 for item in *; do
     if [[ ! -d $item ]]; then
         continue
