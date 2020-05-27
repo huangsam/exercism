b22b2b3c56cefe1a337354edfde8d73b850b2077
Samuel Huang <samhuang91@gmail.com>
2020-05-26 22:11:31 -0700

    Optimize allergies in cpp

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -3,26 +3,19 @@
 namespace allergies {
 
     allergy_test::allergy_test(int score) {
-        allergy_score = score % MAX_SCORE;
+        for (int i = 0; i < 8; i++) {
+            if (1 << i & score) {
+                all_allergies.insert(VALUE_PER_ITEM[i]);
+            }
+        }
     }
 
     bool allergy_test::is_allergic_to(const std::string &item) const {
-        int score_one = allergy_score;
-        int score_two = ITEM_VALUES.at(item);
-        while (score_two > 1) {
-            score_one >>= 1;
-            score_two >>= 1;
-        }
-        return (score_one & score_two) == 1;
+        return all_allergies.count(item);
     }
 
     const allergy_set allergy_test::get_allergies() const {
-        allergy_set result{};
-        for (auto it = ITEM_VALUES.begin(); it != ITEM_VALUES.end(); it++) {
-            const std::string item = it->first;
-            if (is_allergic_to(item)) result.insert(item);
-        }
-        return result;
+        return all_allergies;
     }
 
 }  // namespace allergies
--- cpp/allergies/allergies.h
+++ cpp/allergies/allergies.h
@@ -1,7 +1,6 @@
 #if !defined(ALLERGIES_H)
 #define ALLERGIES_H
 
-#include <map>
 #include <string>
 #include <unordered_set>
 
@@ -9,22 +8,14 @@ namespace allergies {
 
     typedef std::unordered_set<std::string> allergy_set;
 
-    constexpr int MAX_SCORE = 256;
-
-    const std::map<std::string, int> ITEM_VALUES{
-        {"eggs", 1},
-        {"peanuts", 2},
-        {"shellfish", 4},
-        {"strawberries", 8},
-        {"tomatoes", 16},
-        {"chocolate", 32},
-        {"pollen", 64},
-        {"cats", 128}
+    const std::string VALUE_PER_ITEM[8]{
+        "eggs", "peanuts", "shellfish", "strawberries",
+        "tomatoes", "chocolate", "pollen", "cats"
     };
 
     class allergy_test {
     private:
-        int allergy_score;
+        allergy_set all_allergies;
     public:
         allergy_test(int score);
         bool is_allergic_to(const std::string &item) const;
