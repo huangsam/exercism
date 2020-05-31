7829fa8edabc9491252eec6a013bc5a7592abc99
Samuel Huang <samhuang91@gmail.com>
2020-05-31 16:11:20 -0700

    Reformat resistor-color in js

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,6 +3,6 @@ export const colorCode = (color) => {
 };
 
 export const COLORS = [
-    "black", "brown", "red", "orange", "yellow",
-    "green", "blue", "violet", "grey", "white",
+  "black", "brown", "red", "orange", "yellow",
+  "green", "blue", "violet", "grey", "white",
 ];
