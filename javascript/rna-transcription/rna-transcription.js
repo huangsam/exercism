f6b8f4718501b10c59cce772b34ea2e902d4c77a
Samuel Huang <samhuang91@gmail.com>
2020-05-31 17:31:41 -0700

    Reformat rna-transcription in js

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,10 +1,10 @@
 const TOKEN_MAP = {
-    'G': 'C',
-    'C': 'G',
-    'T': 'A',
-    'A': 'U',
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U',
 };
 
 export const toRna = (seq) => {
-    return seq.split('').map(ch => TOKEN_MAP[ch]).join('');
+  return seq.split('').map(ch => TOKEN_MAP[ch]).join('');
 };
