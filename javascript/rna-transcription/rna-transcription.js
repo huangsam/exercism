8e4a62fa6d6b16a9d29022080a2544f53784b168
Samuel Huang <samhuang91@gmail.com>
2020-05-31 17:30:42 -0700

    Implement rna-transcription in js

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,10 @@
+const TOKEN_MAP = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U',
+};
+
+export const toRna = (seq) => {
+    return seq.split('').map(ch => TOKEN_MAP[ch]).join('');
+};
