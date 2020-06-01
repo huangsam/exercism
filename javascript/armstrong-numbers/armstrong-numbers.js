8392381452a2fee46052d274f212677ad7ffe029
Samuel Huang <samhuang91@gmail.com>
2020-05-31 17:20:11 -0700

    Implement armstrong-numbers in js

---
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -0,0 +1,9 @@
+export const isArmstrongNumber = (n) => {
+  const power = Math.floor(Math.log10(n) + 1);
+  let [remainder, sum] = [n, 0];
+  while (remainder) {
+    sum += Math.pow(remainder % 10, power);
+    remainder = Math.floor(remainder / 10);
+  }
+  return sum == n;
+};
