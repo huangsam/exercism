e39594a7ff32829bcf3fe11f24a7b86017fcff0b
Samuel Huang <samhuang91@gmail.com>
2020-12-06 16:00:31 -0800

    Revise to IntStream solution for learning

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -3,12 +3,13 @@ class MicroBlog {
     private static final int MAX_CHARACTERS = 5;
 
     public String truncate(String input) {
-        int numOfPoints = input.codePointCount(0, input.length());
-        if (numOfPoints < MAX_CHARACTERS) {
-            return input;
-        }
-        int offset = input.offsetByCodePoints(0, MAX_CHARACTERS);
-        return input.substring(0, offset);
+        return input.codePoints()
+                .limit(MAX_CHARACTERS) // shorten codePoints stream
+                .collect(
+                        StringBuilder::new, // supply constructor
+                        StringBuilder::appendCodePoint, // create partial results
+                        StringBuilder::append // combine partial results
+                ).toString();
     }
 
 }
